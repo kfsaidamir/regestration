9e8acf6edc5dd626a4395a3606ac9b8b0a58de12
kf_saidamir <skadirov2009@gmail.com>
2024-03-12 22:27:29 +0500

    exe

---
+++ app.js
@@ -0,0 +1,28 @@
+const a = prompt('what is your name?');
+if(a == null) {
+    for(let i = 0; i < 2; i++) {
+     alert('Please enter your name');
+      const a = prompt('what is your name?');
+ }
+}
+const b = +prompt('How old are you?');
+// if(b != Number) {
+//     for(let i = 0; i < 2 ; i++) {
+//     alert('Please enter your age');
+//     const b = +prompt('How old are you?');
+//     }
+// }
+const c = prompt('Your email adress');
+const d = prompt('Password');
+ 
+alert(`welcome  ${a} !`)
+
+for( let i = 0; i < 2 ; i++) {
+    const a = prompt('what is your name?');
+const b = +prompt('How old are you?');
+const c = prompt('Your email adress');
+const d = prompt('Password');
+
+alert(`welcome  ${a} !`)
+
+}
